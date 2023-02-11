50fd22492943d3282aaeb2d9642b93d83c7f986d
Shubham-Rasal <bluequbits@gmail.com>
2023-02-11 11:10:33 +0530

    string clustering

--- api/cluster-by-single-param.js
+++ api/cluster-by-single-param.js
@@ -109,150 +109,224 @@ async function clusterByNumber(name, radius, no_of_neighbours) {
 }
 
 async function clusterByString(name, radius, no_of_neighbours) {
+  // get all entities
+  const entities = await getAllEntities();
+  // get all params
+  const params = await getAllParams();
+  // get the required param
+  const param = params.find((param) => param.name === name);
 
-    // get all entities
-    const entities = await getAllEntities();
-    // get all params
-    const params = await getAllParams();
-    // get the required param
-    const param = params.find((param) => param.name === name);
+  // filter entities to get only the required parameter
+  const filteredEntities = entities.map(({ entity, id }) => {
+    // check if the entity has the required parameter
+    const param = entity.parameters.find((param) => param.name === name);
+    if (!param) {
+      return null;
+    }
+    // return the entity with the value of the required parameter
+    return {
+      id: id,
+      value: param.value,
+    };
+  });
 
-    // filter entities to get only the required parameter
-    const filteredEntities = entities.map(({ entity, id }) => {
-        // check if the entity has the required parameter
-        const param = entity.parameters.find((param) => param.name === name);
-        if (!param) {
-            return null;
+  // filter out the null values
+  const filteredEntities2 = filteredEntities.filter(
+    (entity) => entity !== null
+  );
+
+  //sort in lexicographical order
+  filteredEntities2.sort((a, b) => {
+    if (a.value < b.value) {
+      return -1;
+    }
+    if (a.value > b.value) {
+      return 1;
+    }
+    return 0;
+  });
+
+  console.log("filteredEntities2", filteredEntities2);
+
+  //calc with each string
+  // remove the first element after mapping
+  //continue with the next element
+
+  const distance = [];
+
+  for (let i = 0; i < filteredEntities2.length; i++) {
+    const dis = [];
+
+    for (let j = i; j < filteredEntities2.length; j++) {
+      dis.push(
+        levenshteinDistance(
+          filteredEntities2[i].value,
+          filteredEntities2[j].value
+        )
+      );
+    }
+
+    distance.push(dis);
+  }
+
+  console.log("distance", distance);
+
+  // cluster if distance is less than radius
+
+  const clusters = [];
+
+  for (let i = 0; i < distance.length; i++) {
+    const cluster = [];
+    for (let j = 0; j < distance[i].length; j++) {
+      if (distance[i][j] <= radius) {
+
+        //if already in some cluster, then add to that cluster
+        // else create a new cluster
+
+        if (clusters.length > 0) {
+          let found = false;
+          for (let k = 0; k < clusters.length; k++) {
+            if (clusters[k].includes(filteredEntities2[i].id)) {
+              //check if j is already in the cluster
+              if (!clusters[k].includes(filteredEntities2[j].id)) {
+                clusters[k].push(filteredEntities2[j].id);
+              }
+              found = true;
+              break;
+            }
+          }
+
+          if (!found) {
+            cluster.push(filteredEntities2[i].id);
+            cluster.push(filteredEntities2[j + i].id);
+          }                
+
         }
-        // return the entity with the value of the required parameter
-        return {
-            id: id,
-            value: param.value,
-        };
-    });
-
-    // filter out the null values
-    const filteredEntities2 = filteredEntities.filter((entity) => entity !== null);
-
-    //sort in lexicographical order
-    filteredEntities2.sort((a, b) => {
-        if (a.value < b.value) {
-            return -1;
+        else {
+          cluster.push(filteredEntities2[i].id);
+          cluster.push(filteredEntities2[j + i].id);
         }
-        if (a.value > b.value) {
-            return 1;
-        }
-        return 0;
-    });
+        
+      }
+    }
+    if (cluster.length > 0) {
+      clusters.push(cluster);
+    }
 
-    console.log("filteredEntities2", filteredEntities2);
+  }
 
-    // create a dataset based on levenstein distance
-    const dataset = filteredEntities2.map((entity, index) => {
-        const dis = index === 0 ? 0 : levenshtein(entity.value, filteredEntities2[index - 1].value);
-        return [dis, 0];
-    });
+  //remove duplicates
+  for (let i = 0; i < clusters.length; i++) {
+    clusters[i] = [...new Set(clusters[i])];
+  }
 
-    console.log("dataset", dataset);
+  console.log("clusters", clusters);
+
+
 }
 
 async function clusterByBoolean(name, radius, no_of_neighbours) {
-    // get all entities
-    const entities = await getAllEntities();
-    // get all params
-    const params = await getAllParams();
-    // get the required param
-    const param = params.find((param) => param.name === name);
+  // get all entities
+  const entities = await getAllEntities();
+  // get all params
+  const params = await getAllParams();
+  // get the required param
+  const param = params.find((param) => param.name === name);
 
-    // filter entities to get only the required parameter
-    const filteredEntities = entities.map(({ entity, id }) => {
-        // check if the entity has the required parameter
-        if(!entity.parameters){
-            return null;
-        }
+  // filter entities to get only the required parameter
+  const filteredEntities = entities.map(({ entity, id }) => {
+    // check if the entity has the required parameter
+    if (!entity.parameters) {
+      return null;
+    }
 
-        const param = entity.parameters.find((param) => param.name === name);
+    const param = entity.parameters.find((param) => param.name === name);
 
-        if (!param) {
-            return null;
-        }
+    if (!param) {
+      return null;
+    }
 
-        // return the entity with the value of the required parameter
-        return {
-            id: id,
-            value: param.value,
-        };
-    });
+    // return the entity with the value of the required parameter
+    return {
+      id: id,
+      value: param.value,
+    };
+  });
 
-    // filter out the null values
-    const filteredEntities2 = filteredEntities.filter((entity) => entity !== null);
+  // filter out the null values
+  const filteredEntities2 = filteredEntities.filter(
+    (entity) => entity !== null
+  );
 
-    //sort in two clusters based on true or false
-    filteredEntities2.sort((a, b) => {
-        if (a.value < b.value) {
-            return -1;
-        }
-        if (a.value > b.value) {
-            return 1;
-        }
+  //sort in two clusters based on true or false
+  filteredEntities2.sort((a, b) => {
+    if (a.value < b.value) {
+      return -1;
+    }
+    if (a.value > b.value) {
+      return 1;
+    }
 
-        return 0;
-    });
+    return 0;
+  });
 
-    console.log("filteredEntities2", filteredEntities2);
+  console.log("filteredEntities2", filteredEntities2);
 
-    //create a map of clusters
-    const clustersMap = new Map();
-    clustersMap.set("true", []);
-    clustersMap.set("false", []);
+  //create a map of clusters
+  const clustersMap = new Map();
+  clustersMap.set("true", []);
+  clustersMap.set("false", []);
 
-    // add entities to clustermap
-    filteredEntities2.forEach((entity) => {
-        if (entity.value) {
-            clustersMap.get("true").push(entity.id);
-        } else {
-            clustersMap.get("false").push(entity.id);
-        }
-    });
+  // add entities to clustermap
+  filteredEntities2.forEach((entity) => {
+    if (entity.value) {
+      clustersMap.get("true").push(entity.id);
+    } else {
+      clustersMap.get("false").push(entity.id);
+    }
+  });
 
-    console.log("clustersMap", clustersMap);
+  console.log("clustersMap", clustersMap);
 
-    return clustersMap;
+  return clustersMap;
 }
 
-function levenshtein(a, b) {
-    if (a.length === 0) return b.length;
-    if (b.length === 0) return a.length;
+function levenshteinDistance(a, b) {
+  if (a.length === 0) return b.length;
+  if (b.length === 0) return a.length;
 
-    var matrix = [];
+  var matrix = [];
 
-    // increment along the first column of each row
-    var i;
-    for (i = 0; i <= b.length; i++) {
-        matrix[i] = [i];
+  // increment along the first column of each row
+  var i;
+  for (i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  // increment each column in the first row
+  var j;
+  for (j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  // Fill in the rest of the matrix
+  for (i = 1; i <= b.length; i++) {
+    for (j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) == a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1, // substitution
+          Math.min(
+            matrix[i][j - 1] + 1, // insertion
+            matrix[i - 1][j] + 1
+          )
+        ); // deletion
+      }
     }
+  }
 
-    // increment each column in the first row
-    var j;
-    for (j = 0; j <= a.length; j++) {
-        matrix[0][j] = j;
-    }
-
-    // Fill in the rest of the matrix
-    for (i = 1; i <= b.length; i++) {
-        for (j = 1; j <= a.length; j++) {
-            if (b.charAt(i - 1) == a.charAt(j - 1)) {
-                matrix[i][j] = matrix[i - 1][j - 1];
-            } else {
-                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, // substitution
-                    Math.min(matrix[i][j - 1] + 1, // insertion
-                        matrix[i - 1][j] + 1)); // deletion
-            }
-        }
-    }
-
-    return matrix[b.length][a.length];
+  return matrix[b.length][a.length];
 }
-
 
 module.exports = clusterBySingleParam;
